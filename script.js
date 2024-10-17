32184ce4854ab4d386bfc9dd4478f7cf825daca4
1Andria <andriamelua22@gmail.com>
2024-10-17 17:22:36 +0400

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = "ანდრია";
+const lastname = " მელუა ";
+let age = 18;
+const mail = " andriamelua22@gmail.com";
+const phone = 557399184;
+const adulthood = true;
+console.log(name + lastname + age + mail);
+console.warn(adulthood);
+console.error(phone);
